e7fa7c9f16098b9af32b764e9a91213cc2dcdaee
Slumpy_666 <dr.slumpy.666@gmail.com>
2026-04-27 03:31:38 -0400

    fixed hero

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const roleText = 'Systems & Full-Stack Engineer | CS @ VT'
 
-export const heroBio = 'Systems programmer and full-stack developer with a focus on low-level security, infrastructure. I like building things that I\'ll end up using.'
+export const heroBio = 'Systems programmer and full-stack developer with a focus on low-level security, infrastructure. I like building things that entertain me'
 
-export const currentProject = { label: 'Garage64', href: '#projects' }
+export const currentLocation = { label: 'Ashburn, VA'}
