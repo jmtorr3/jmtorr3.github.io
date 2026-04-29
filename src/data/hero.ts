59caf2c57ed8b5a372517fa6faf17f4eceea235f
Slumpy_666 <dr.slumpy.666@gmail.com>
2026-04-29 01:54:57 -0400

    hero update

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,4 +1,4 @@
-export const roleText = 'Systems & Full-Stack Engineer | CS @ VT'
+export const roleText = 'Systems & Full-Stack Engineer'
 
 export const heroBio = 'Systems programmer and full-stack developer with a focus on low-level security, infrastructure. I like building things that entertain me.'
 
