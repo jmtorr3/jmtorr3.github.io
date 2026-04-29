7b2eaa4ed181ad2328606fe00e9df0771d5448af
Slumpy_666 <dr.slumpy.666@gmail.com>
2026-04-29 11:33:35 -0400

    link to tim's portfolio

--- src/data/other.ts
+++ src/data/other.ts
@@ -9,7 +9,7 @@ export const otherProjects = [
     github: 'https://github.com/jmtorr3/Garage32',
     logo: `${BASE}/logos/Garage32.png`,
     description: 'A custom Minecraft texture pack that transforms boats into fully modeled cars using Custom Entity Models (CEM). Multiple car models across wood type variants — designed in collaboration with',
-    collaborator: { name: 'Tim Kang', href: 'https://www.instagram.com/slug_7/' },
+    collaborator: { name: 'Tim Kang', href: 'https://docs.google.com/presentation/d/1vWJu3uwU5GVb_wxYL7KUJRgU9V8TJZpSqgTa2liIfHw/edit?slide=id.g3cb7c89ca29_0_18#slide=id.g3cb7c89ca29_0_18' },
     previews: [
       { src: `${BASE}/gifs/TakumiAE86.gif`, alt: 'Takumi AE86' },
     ],
