f4edf33d0d994cc852cafc6da81f2564fe52f70c
jmtorr3 <dr.slumpy.666@gmail.com>
2026-05-08 00:11:58 -0400

    updated title

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
-export const roleText = 'Systems & Full-Stack Engineer'
+export const roleText = 'Systems & Security Engineer'
 
-export const heroBio = 'Systems programmer and full-stack developer with a focus on low-level security, infrastructure. I like building things that entertain me.'
+export const heroBio = 'Systems programmer and full-stack developer with a focus on low-level security and infrastructure. I like building things that entertain me.'
 
 export const currentLocation = { label: 'Ashburn, VA'}
